c657f7a648ec4d5c2be2f43d410cf44aa1bf428e
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-06 17:27:23 +0530

    used Fetch on fake api post request

--- script.js
+++ script.js
@@ -33,6 +33,36 @@ function getData(){
 }
 
 
-getData()
+//getData()
+
+
+//post request
+//the fetch in post request also takes another parameter other than the url
+function postData(){
+    url = "https://jsonplaceholder.typicode.com/posts"   //fake dummy api to create post request
+    data = {  //we are sending this data there
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      }
+    params ={
+        method: 'post',  //signifies it's a post request 
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8' //content type mostly is application/json
+        },
+        body: JSON.stringify(data)  //-to be used if data is json not string
+        //body: data - when data is stting
+    }
+    fetch(url,params).then((response)=>{
+        //return response.text()   this will print out the list as text
+        return response.json()  //will return is json format which is an array of objects
+    }).then((data)=>{    
+        console.log(data)  //printed the data
+    })
+}
+
+
+
+postData()
 
 
